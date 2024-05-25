9a0c2ee65c8efbc654f130511dafacf34d1c0320
LuoTat <LuoTat.s@gmail.com>
2024-05-25 20:58:52 +0800

    添加了ArrayList_G的测试

--- test/src/ArrayListTest/ArrayListTest.cpp
+++ test/src/ArrayListTest/ArrayListTest.cpp
@@ -3,6 +3,11 @@
 #include <time.h>
 #include <vector>
 #include "ArrayList.h"
+#include "ArrayList_G.h"
+
+#define INTCMP(a, b) ((a) == (b))
+
+LTT_ARRAYLIST_INIT(INT, int, INTCMP)
 
 using namespace std;
 
@@ -17,8 +22,10 @@ void PRINT(ArrayList* ArrayList)
 
 int main()
 {
-    clock_t     start, end;
-    ArrayList*  ArrayList = LTT_ArrayList_New(sizeof(int), NULL);
+    clock_t    start, end;
+    ArrayList* ArrayList = LTT_ArrayList_New(sizeof(int), NULL);
+    ArrayList(INT) ArrayList_G;
+    ArrayList_Init(INT, &ArrayList_G);
     vector<int> vector;
     int*        array = (int*)malloc(NUMBER * sizeof(int));
     for (int i = 0; i < NUMBER; i++) { array[i] = i; }
@@ -27,6 +34,11 @@ int main()
     for (int i = 0; i < NUMBER; i++) { LTT_ArrayList_AddLast(ArrayList, &array[i]); }
     end = clock();
     printf("LTT_ArrayList 尾部插入%d个元素耗时: %.4lf ms\n", NUMBER, (double)(end - start) / CLOCKS_PER_SEC * 1000);
+
+    start = clock();
+    for (int i = 0; i < NUMBER; i++) { ArrayList_AddLast(INT, &ArrayList_G, array[i]); }
+    end = clock();
+    printf("ArrayList_G   尾部插入%d个元素耗时: %.4lf ms\n", NUMBER, (double)(end - start) / CLOCKS_PER_SEC * 1000);
 
     start = clock();
     for (int i = 0; i < NUMBER; i++) { vector.push_back(array[i]); }
