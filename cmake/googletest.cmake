713464c05cf17f7b13585c2035f4c8ec73a89092
LuoTat <LuoTat.s@gmail.com>
2025-03-02 13:24:58 +0800

    临时提交

--- cmake/googletest.cmake
+++ cmake/googletest.cmake
@@ -1,13 +1,8 @@
-include(FetchContent)
+
 
 FetchContent_Declare(
-    googletest
-    GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG main
-    SOURCE_DIR ${CMAKE_SOURCE_DIR}/3rdparty/googletest
+googletest
+GIT_REPOSITORY https://github.com/google/googletest.git
+# release-1.11.0
+GIT_TAG e2239ee6043f73722e7aa812a459f54a28552929
 )
-
-# For Windows: Prevent overriding the parent project's compiler/linker settings
-set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-
-FetchContent_MakeAvailable(googletest)
